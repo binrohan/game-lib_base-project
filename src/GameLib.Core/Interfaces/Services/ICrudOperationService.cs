f38bdd82e30b2485c18a7da2fc4a1179c81486ef
Bin Rohan <binrohan.cs@gmail.com>
2024-01-17 18:04:15 +0600

    implemented generic crud operation service

---
+++ src/GameLib.Core/Interfaces/Services/ICrudOperationService.cs
@@ -0,0 +1,10 @@
+﻿namespace GameLib.Core.Interfaces.Services;
+
+public interface ICrudOperationService<TEntity, TCreateDto, TReturnDto, TUpdateDto>
+{
+    Task<TEntity> AddAndSaveAsync(TCreateDto dto);
+    Task<IEnumerable<TReturnDto>> GetAllAsync();
+    Task<TReturnDto> GetById(int id);
+    Task<TEntity> UpdateAndSaveAsync(int id, TUpdateDto dto);
+    Task<int> Delete(int id);
+}
---
+++ src/GameLib.Core/Services/CrudOpserationService.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using GameLib.Core.Exceptions;
+using GameLib.Core.Interfaces;
+using GameLib.Core.Interfaces.Repositories;
+using GameLib.Core.Interfaces.Services;
+
+namespace GameLib.Core.Serivces;
+
+public class CrudOpserationService<TEntity, TCreateDto, TReturnDto, TUpdateDto>(IUnitOfWork unitOfWork, IMapper mapper) 
+    : ICrudOperationService<TEntity, TCreateDto, TReturnDto, TUpdateDto> 
+    where TEntity : class 
+    where TCreateDto : class
+    where TReturnDto : class
+    where TUpdateDto : class
+{
+    private readonly IUnitOfWork _unitOfWork = unitOfWork;
+    private readonly IMapper _mapper = mapper;
+    private readonly IRepository<TEntity> _repo = unitOfWork.Repository<TEntity>();
+
+    public virtual async Task<TEntity> AddAndSaveAsync(TCreateDto dto)
+    {
+        var entity = _mapper.Map<TEntity>(dto);
+
+        return await _repo.AddAndSaveAsync(entity);
+    }
+
+    public virtual async Task<IEnumerable<TReturnDto>> GetAllAsync()
+    {
+        var entities = await _repo.GetAllAsync();
+
+        return _mapper.Map<IEnumerable<TReturnDto>>(entities);
+    }
+
+    public virtual async Task<TReturnDto> GetById(int id)
+    {
+        var entity = await _repo.GetByIdAsync(id) ?? throw new NotFoundException();
+
+        return _mapper.Map<TReturnDto>(entity);
+    }
+
+    public virtual async Task<TEntity> UpdateAndSaveAsync(int id, TUpdateDto dto)
+    {
+        var entity = await _repo.GetByIdAsync(id) ?? throw new NotFoundException();
+
+        _mapper.Map(dto, entity);
+
+        return await _repo.UpdateAndSaveAsync(entity);
+    }
+
+    public virtual async Task<int> Delete(int id)
+    {
+        var entity = await _repo.GetByIdAsync(id) ?? throw new NotFoundException();
+
+        return await _repo.DeleteAsync(id);
+    }
+}

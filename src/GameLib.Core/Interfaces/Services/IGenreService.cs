7a978875654e28b415aa7face75b0969ff9fd96d
Bin Rohan <binrohan.cs@gmail.com>
2024-01-18 14:02:55 +0600

    created genre service

---
+++ src/GameLib.Core/Interfaces/Services/IGenreService.cs
@@ -0,0 +1,8 @@
+﻿using GameLib.Domain.Entities;
+
+namespace GameLib.Core.Interfaces.Services;
+
+public interface IGenreService : ICrudOperationService<Genre, Genre, Genre, Genre>
+{
+
+}
---
+++ src/GameLib.Core/Services/GenreService.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using GameLib.Core.Interfaces;
+using GameLib.Core.Interfaces.Services;
+using GameLib.Domain.Entities;
+
+namespace GameLib.Core.Serivces;
+
+public class GenreService(IUnitOfWork unitOfWork, IMapper mapper) 
+  : CrudOpserationService<Genre, Genre, Genre, Genre>(unitOfWork, mapper), 
+    IGenreService
+{
+
+}

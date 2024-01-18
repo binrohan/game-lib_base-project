10d53f269a76d000660b49dfa4473d62e8e5fa4e
Bin Rohan <binrohan.cs@gmail.com>
2024-01-18 16:13:31 +0600

    changed return type of results

--- src/GameLib.API/ApiResponses/Result.cs
+++ src/GameLib.API/ApiResponses/Result.cs
@@ -7,7 +7,6 @@ public class Result
 {
     public static ActionResult<ApiResponse> Ok(object details, string? message = null) => new OkObjectResult(new ApiResponse(200, details, message));
     public static ActionResult<ApiResponse> Created(object details, string? message = null) => new CreatedResult("", new ApiResponse(201, details, message));
-    public static ActionResult Updated() => new NoContentResult();
-    public static ActionResult Patched() => new NoContentResult();
-    public static ActionResult Deleted() => new OkObjectResult(new ApiResponse(200, "Delete successed"));
+    public static ActionResult<ApiResponse> Updated(object? details = null, string? message = null) => new OkObjectResult(new ApiResponse(204, details, message));
+    public static ActionResult<ApiResponse> Deleted() => new OkObjectResult(new ApiResponse(200, "Delete successed"));
 }

5a0aa4b21b354307da6809342e78915c1a2affc1
Bin Rohan <binrohan.cs@gmail.com>
2024-01-18 16:17:19 +0600

    changed generic types in interface

--- src/GameLib.Core/Interfaces/Services/ICrudOperationService.cs
+++ src/GameLib.Core/Interfaces/Services/ICrudOperationService.cs
@@ -4,7 +4,7 @@ public interface ICrudOperationService<TEntity, TCreateDto, TReturnDto, TUpdateD
 {
     Task<TEntity> AddAndSaveAsync(TCreateDto dto);
     Task<IEnumerable<TReturnDto>> GetAllAsync();
-    Task<TReturnDto> GetByIdAsync(int id);
+    Task<TEntity> GetByIdAsync(int id);
     Task<TEntity> UpdateAndSaveAsync(int id, TUpdateDto dto);
     Task<int> DeleteAsync(int id);
 }

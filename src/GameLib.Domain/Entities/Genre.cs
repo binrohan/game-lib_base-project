8a1874464414c59edb09dc8d64e7cebcb460eab0
Bin Rohan <binrohan.cs@gmail.com>
2024-01-18 14:02:23 +0600

    navigation properties initialized with new syntax

--- src/GameLib.Domain/Entities/Genre.cs
+++ src/GameLib.Domain/Entities/Genre.cs
@@ -4,6 +4,6 @@ public class Genre : AuditableEntity
 {
     public int Id { get; set; }
     public required string Name { get; set; }
-    public ICollection<Game> Games { get; set; } = new List<Game>();
-    public ICollection<GameGenre> GameGenres { get; set; } = new List<GameGenre>();
+    public ICollection<Game> Games { get; set; } = [];
+    public ICollection<GameGenre> GameGenres { get; set; } = [];
 }

e812a1589781156126d19939c85cbed117e9790b
Bin Rohan <binrohan.cs@gmail.com>
2024-01-18 14:56:55 +0600

    http response result abstracted

---
+++ src/GameLib.API/ApiResponses/Result.cs
@@ -0,0 +1,13 @@
+﻿using GameLib.Core.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameLib.API;
+
+public class Result
+{
+    public static ActionResult<ApiResponse> Ok(object details, string? message = null) => new OkObjectResult(new ApiResponse(200, details, message));
+    public static ActionResult<ApiResponse> Created(object details, string? message = null) => new CreatedResult("", new ApiResponse(201, details, message));
+    public static ActionResult Updated() => new NoContentResult();
+    public static ActionResult Patched() => new NoContentResult();
+    public static ActionResult Deleted() => new OkObjectResult(new ApiResponse(200, "Delete successed"));
+}

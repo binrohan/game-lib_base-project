74cf8d50cfd089ee8ca51bd58dd43857ae91f64e
Bin Rohan <binrohan.cs@gmail.com>
2024-01-22 18:37:11 +0600

    controller route naming convention fixed #12

---
+++ src/GameLib.API/Infrastructure/KebabCasingTransformer.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.RegularExpressions;
+
+namespace GameLib.API.Infrastructure;
+
+public class KebabCasingTransformer : IOutboundParameterTransformer
+{
+    public string? TransformOutbound(object? value) => value != null 
+        ? Regex.Replace(value.ToString()!, "([a-z])([A-Z])", "$1-$2").ToLower()
+        : null;
+}
--- src/GameLib.API/Program.cs
+++ src/GameLib.API/Program.cs
@@ -2,12 +2,13 @@ using GameLib.API.Extensions;
 using GameLib.API.Infrastructure;
 using GameLib.Infrastructure.Data;
 using GameLib.Core.Exceptions;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt => opt.Conventions.Add(new RouteTokenTransformerConvention(new KebabCasingTransformer())));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
